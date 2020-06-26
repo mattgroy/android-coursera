8613a83749daba3d92bbab2cd40424dac700547f
Max Ratkov <ratkovm8@gmail.com>
2020-06-26 19:33:41 +0500

    C1-3.3 Валидация email и password. Ошибки в Toast

--- app/src/main/java/com/mattgroy/FirstApplication/AuthActivity.java
+++ app/src/main/java/com/mattgroy/FirstApplication/AuthActivity.java
@@ -1,11 +1,15 @@
 package com.mattgroy.FirstApplication;
 
+import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class AuthActivity extends AppCompatActivity {
 
@@ -17,7 +21,11 @@ public class AuthActivity extends AppCompatActivity {
     private View.OnClickListener onEnterClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            //TODO Обработка нажатий
+            if (isEmailValid() && isPasswordValid()) {
+                //TODO Переход в приложение
+            } else {
+                showMessage(R.string.auth_input_error);
+            }
         }
     };
     private View.OnClickListener onRegisterClickListener = new View.OnClickListener() {
@@ -26,6 +34,19 @@ public class AuthActivity extends AppCompatActivity {
             //TODO Обработка нажатий
         }
     };
+
+    private boolean isEmailValid() {
+        return !TextUtils.isEmpty(etLogin.getText()) &&
+                Patterns.EMAIL_ADDRESS.matcher(etLogin.getText()).matches();
+    }
+
+    private void showMessage(@StringRes int messageId) {
+        Toast.makeText(this, messageId, Toast.LENGTH_LONG).show();
+    }
+
+    private boolean isPasswordValid() {
+        return !TextUtils.isEmpty(etPassword.getText());
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
