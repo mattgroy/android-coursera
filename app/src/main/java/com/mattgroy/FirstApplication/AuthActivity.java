818bc481f9195d4ba1e9d97f7aa8aafb6757ba5b
Max Ratkov <ratkovm8@gmail.com>
2020-06-26 19:38:07 +0500

    Reformat Code

--- app/src/main/java/com/mattgroy/FirstApplication/AuthActivity.java
+++ app/src/main/java/com/mattgroy/FirstApplication/AuthActivity.java
@@ -1,8 +1,5 @@
 package com.mattgroy.FirstApplication;
 
-import androidx.annotation.StringRes;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Patterns;
@@ -10,6 +7,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.annotation.StringRes;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class AuthActivity extends AppCompatActivity {
 
